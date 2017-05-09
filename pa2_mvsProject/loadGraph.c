f67b45c52543a4dcb7724f7acce35d5adaf0eefa
FerociousEwok <thisisforxbox15@gmail.com>
2017-05-09 14:59:30 -0700

    deleted comments

--- pa2_mvsProject/loadGraph.c
+++ pa2_mvsProject/loadGraph.c
@@ -123,41 +123,7 @@ IntVec* loadGraph(FILE *inputFile, int nodeCount, char* flag) //Done
 	}
 	return tempList;
 }
-/* 
-IntVec* loadGraphUndirected(FILE *inputFile, int nodeCount) //edges go both ways; for each edge, edgeCount+=2.
-{
-	//local variables
-	int nodeCount = 0, tempInt = 0, dataValue = 0;
-	float weight;
-	IntVec *tempList;
-	char *lineOfFile, *tempToken, *tempDataValue, *tempWeight;
 
-	//nodeCount = getNodeCount(inputFile);
-	tempList = calloc(nodeCount + 1, sizeof(IntVec));
-	for (int i = 0; i <= nodeCount; i++)
-		tempList[i] = intMakeEmptyVec();
-	//below is some input file cleanup
-	for (int i = 0; i < 1; i++)
-		fgets(lineOfFile, 20, inputFile);
-	while ((fgets(lineOfFile, 20, inputFile) != NULL)) //for each line of the file.
-	{
-		if (lineOfFile[0] == '\n') //if fgets needed to clear newline character
-			fgets(lineOfFile, 20, inputFile);
-		sscanf(lineOfFile, "%s %s %s", tempToken, tempDataValue, tempWeight);
-		//get first token
-		tempInt = (int)tempToken[0] - (int)'0';
-		//get second token
-		dataValue = (int)tempDataValue[0] - (int)'0';
-		//if there is a weight
-		if (!((float)tempWeight[0] == 0.00))
-			weight = (float)tempWeight[0] - (float)'0';
-
-		intVecPush(tempList[tempInt], dataValue);//edge from tempInt vector to dataValue Vector.
-		intVecPush(tempList[dataValue], tempInt);//edge from dataValue vector to tempInt vector.
-	}
-	return tempList;
-}
-*/
 int getNodeCount(FILE *inputFile) //Only call once or there might be errors.
 {
 	char *tempString;
--- pa2_mvsProject/loadGraph.h
+++ pa2_mvsProject/loadGraph.h
@@ -7,29 +7,38 @@ TODO: change all function parameters to not be pointer types.
 
 #ifndef C101loadGraph
 #define C101loadGraph
-/**/
+/*
+
+*/
 int** makeAdjMatrix(IntVec *adjList, int nodeCount);
 
-/**/
+/*
+
+*/
 IntVec* transposeGraph(IntVec *adjList, int n);
 
-/**/
+/*
+
+*/
 void printAdjVerts(IntVec *adjList, int nodeCount);
 
 /**/
 void printAdjMatrix(int **adjMatrix, int nodeCount);
 
-/**/
+/*
+
+*/
 
 IntVec* loadGraph(FILE *inputFile, int nodeCount, char *flag);
 
-/**/
+/*
+
+*/
 int getNodeCount(FILE *inputFile);
 
-/**/
-//IntVec* loadGraphUndirected(FILE *inputFile, int nodeCount);
+/*
 
-/**/
+*/
 int getEdgeCount(IntVec *adjList);
 
 #endif
